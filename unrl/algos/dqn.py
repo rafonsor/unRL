46e0daf1a4e4d8f913e3b3af79bd798af18045cf
Rafael Afonso Rodrigues <rafael.ar@live.be>
2023-10-27 18:08:03 +0100

    feat: Deep Q-Network with Experience Replay

--- unrl/algos/dqn.py
+++ unrl/algos/dqn.py
@@ -20,7 +20,7 @@ from torch.optim import SGD
 import unrl.types as t
 from unrl.config import validate_config
 from unrl.action_sampling import EpsilonGreedyActionSampler
-from unrl.containers import ContextualTransition, ContextualTrajectory
+from unrl.containers import ContextualTransition, ContextualTrajectory, ExperienceBuffer
 from unrl.utils import persisted_generator_value
 
 
@@ -116,3 +116,46 @@ class DQN:
         self._optim.step()
         self._optim.zero_grad()
         return loss.item()
+
+
+class DQNExperienceReplay(DQN):
+    """Deep Q-Network with an Experience Replay buffer as originally proposed by [1]_.
+
+    References:
+        [1] Mnih, V. & Kavukcuoglu, K. & Silver, D., et al. (2015). "Human-level control through deep reinforcement
+            learning". Nature, 518, pp. 529-533.
+    """
+    def __init__(self,
+                 action_value_model: pt.nn.Module,
+                 *,
+                 learning_rate: float,
+                 discount_factor: float,
+                 epsilon_greedy: float,
+                 target_refresh_steps: int,
+                 replay_memory_capacity: int,
+                 batch_size: int):
+        super().__init__(action_value_model, learning_rate=learning_rate, discount_factor=discount_factor,
+                         epsilon_greedy=epsilon_greedy, target_refresh_steps=target_refresh_steps)
+        self.experience_buffer = ExperienceBuffer(maxlen=replay_memory_capacity)
+        self.batch_size = batch_size
+
+    def _compute_td_error(self, action_values: pt.Tensor, transition: ContextualTransition) -> pt.Tensor:
+        """Compute the One-step TD-errors for a minibatch of sampled experiences. The experienced SARST transition is
+        added to the Experience buffer beforehand.
+
+        Action-value estimates for all starting states are recomputed on the spot using the behaviour model.
+
+        Args:
+            action_values: (NotUsed)
+            transition: SARST transition to add to the Experience buffer.
+
+        Returns:
+            One-step TD-errors for each sampled transition as a unidimensional tensor
+        """
+        # Save experienced transition and sample a minibatch to replay
+        self.experience_buffer.append(*transition)
+        batch = self.experience_buffer.sample(self.batch_size)
+        # Compute One-step TD-errors for all samples
+        targets = batch['reward']
+        targets += (1 - batch['terminal']) * self.discount_factor * self.target_model(batch['next_state']).max(dim=-1)
+        return targets - pt.take_along_dim(self.behaviour_model(batch['state']), batch['action'])
